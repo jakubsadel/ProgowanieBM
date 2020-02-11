f9d4bc73b69abb656de6fe989a20ba707d06134a
jakubsadel <kubasadel97@gmail.com>
2020-02-12 00:29:16 +0100

    Add files via upload
    asm poprawka

--- JAAsm/JAAsm.asm
+++ JAAsm/JAAsm.asm
@@ -1,8 +1,6 @@
 
 .data
 
-three REAL8 3.0
-threshold REAL8 127.0
 
 .code
 
@@ -18,18 +16,35 @@ generateasm PROC
 
 	emms										; przygotowanie koprocesora do dzia³añ na wartoœciach zmiennoprzecinkowych
 
+	mov		eax, 150							;Wektor z progiem binaryzacji
+	pinsrd xmm4, eax, 0							
+	pinsrd xmm4, eax, 1
+	pinsrd xmm4, eax, 2
+	pinsrd xmm4, eax, 3
+
+
+	mov	eax, 255								;Wektor z mnoznikiem
+	pinsrd xmm5, eax, 0						
+	pinsrd xmm5, eax, 1
+	pinsrd xmm5, eax, 2
+	pinsrd xmm5, eax, 3
+
+
+
 	mov dword ptr [rsp + 18h], edx				; pobiera i zapisuje szerokoœæ 
 	mov byte ptr [rsp + 28h], 0					; padding ( bmp padding ) 
 
 ; if ((width * 3) % 4 != 0)
-	imul eax, dword ptr [rsp + 18h], 3			; width * 3
-	and eax, 3									; (width * 3) % 4
+	imul eax, dword ptr [rsp + 18h], 9			; width * 3
+	and eax, 9									; (width * 3) % 4
 	cmp eax, 0									; ((width * 3) % 4) != 0
 	je falseIf									; skok je¿eli padding jest niepotrzebny
 
 ; padding = 4 - ((width * 3) % 4)
-	mov byte ptr [rsp + 28h], 4					; padding = 4
+	mov byte ptr [rsp + 28h], 12					; padding = 4
 	sub byte ptr [rsp + 28h], al				; padding = 4 - ((width * 3) % 4)
+
+
 
 falseIf:
 	mov r15, 0									; @@@ indeks, kontroluje iterowanie po tablicy w pamiêci @@@ - ustawienie pozycji na 0
@@ -37,42 +52,70 @@ falseIf:
 
 widthLoop:										; pêtla wewnêtrzna 
 
-		movzx rax, byte ptr [rcx + r15 + 2]		; zapisanie do rax wartoœci sk³adowej koloru zielonego piksela prawego obrazu ( wype³nia zerami ró¿nice w pojemnoœci typów )
-		cvtsi2sd xmm6,rax						; konwersja 32-bitowej wartoœci ca³kowitoliczbowej do 64-bitowej wartoœci zmiennoprzecinkowej
 
-		movzx rax, byte ptr [rcx + r15 + 1]		; zapisanie do rax wartoœci sk³adowej koloru zielonego piksela prawego obrazu ( wype³nia zerami ró¿nice w pojemnoœci typów )
-		cvtsi2sd xmm5,rax						; konwersja 32-bitowej wartoœci ca³kowitoliczbowej do 64-bitowej wartoœci zmiennoprzecinkowej
+		movzx eax, byte ptr[rcx + r15]				 ;Wartoœci koloru niebieskiego 4 kolejnych pikseli
+		pinsrd xmm1, eax, 0						 
+		movzx eax, byte ptr[rcx + r15+3]
+		pinsrd xmm1, eax, 1
+		movzx eax, byte ptr[rcx + r15+6]
+		pinsrd xmm1, eax, 2
+		movzx eax, byte ptr[rcx + r15+9]
+		pinsrd xmm1, eax, 3
+
+		movzx eax, byte ptr[rcx + r15+1]			;Wartoœci koloru zielonego 4 kolejnych pikseli
+		pinsrd xmm2, eax, 0						
+		movzx eax, byte ptr[rcx + r15+4]
+		pinsrd xmm2, eax, 1
+		movzx eax, byte ptr[rcx + r15+7]
+		pinsrd xmm2, eax, 2
+		movzx eax, byte ptr[rcx + r15+10]
+		pinsrd xmm2, eax, 3				
+
+
+
+		movzx eax, byte ptr[rcx + r15+2]			;Wartoœci koloru czerwonego 4 kolejnych pikseli
+		pinsrd xmm3, eax, 0							
+		movzx eax, byte ptr[rcx + r15+5]
+		pinsrd xmm3, eax, 1
+		movzx eax, byte ptr[rcx + r15+8]
+		pinsrd xmm3, eax, 2
+		movzx eax, byte ptr[rcx + r15+11]
+		pinsrd xmm3, eax, 3
+
+
+		paddusb xmm1,xmm2							 ; sumowanie wartoœci B z wartoœci¹ G
+		paddusb xmm1,xmm3							 ; sumowanie poprzednio uzyskanej wartoœci z wartoœci¹ R
 		
-		movzx rax, byte ptr [rcx + r15]			; analogicznie do koloru zielonego
-		cvtsi2sd xmm4,rax						; konwersja 32-bitowej wartoœci ca³kowitoliczbowej do 64-bitowej wartoœci zmiennoprzecinkowej
 
-		addsd xmm4,xmm5							; sumowanie wartoœci B z wartoœci¹ G
-		addsd xmm4,xmm6							; sumowanie poprzednio uzyskanej wartoœci z wartoœci¹ R
-		divsd xmm4,three						; dzielenie liczby przez 3 celem uzyskania wartoœci koloru szaroœci
-
-        comisd  xmm4, QWORD PTR threshold		; porówanie otrzymaje wartoœci z wartoœci¹ progowa
-        jbe    SHORT conditionLoop				; skok jesli mniejsze lub równe
+		cmpnltps xmm1, xmm4							 ;porównanie sumy kolorów z wspó³czynikiem zwraca 1 lub 0
+		mulps xmm1, xmm5							 ;mno¿enie razy wektor wype³niony 255
 
 
-		cvttsd2si rax,xmm4						; konwersja 64-bitowej wartoœci zmiennoprzecinkowej do 32-bitowej wartoœci ca³kowitoliczbowej
-		mov byte ptr[rcx + r15],255				; zapisanie bia³ego jako parametr B do tablicy
-		mov byte ptr[rcx + r15 + 1],255			; zapisanie bia³ego jako parametr do tablicy
-		mov byte ptr[rcx + r15 + 2],255			; zapisanie bia³ego jako parametr do tablicy
-	    jmp     SHORT incrLoop					; skok do przesuniêcia tablicy
 
-;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+		pextrd eax, xmm1, 0							 ;zaladowanie 0 lub 255
+		mov byte ptr[rcx + r15], al				
+		mov byte ptr[rcx + r15+1], al
+		mov byte ptr[rcx + r15+2], al
 
-conditionLoop:									; pêtla warunkowa
-		cvttsd2si rax,xmm4						; konwersja 64-bitowej wartoœci zmiennoprzecinkowej do 32-bitowej wartoœci ca³kowitoliczbowej
-		mov byte ptr[rcx + r15],0				; zapisanie czarnego jako parametr B do tablicy
-		mov byte ptr[rcx + r15 + 1],0			; zapisanie czarnego jako parametr G do tablicy
-		mov byte ptr[rcx + r15 + 2],0			; zapisanie czarnego jako parametr R do tablicy
+		pextrd	eax, xmm1, 1				
+		mov	byte ptr[rcx + r15+3], al
+		mov	byte ptr[rcx + r15+4], al
+		mov	byte ptr[rcx + r15+5], al
 
-;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+		pextrd	eax, xmm1, 2					
+		mov	byte ptr[rcx + r15+6], al
+		mov	byte ptr[rcx + r15+7], al
+		mov	byte ptr[rcx + r15+8], al
+		
+		pextrd	eax, xmm1, 3					 
+		mov	byte ptr[rcx + r15+9], al
+		mov	byte ptr[rcx + r15+10], al
+		mov	byte ptr[rcx + r15+11], al
+										
 
-incrLoop:										; pêtla incrementuj¹ca
-		add r15, 3								; przesuniêcie indeksu tablicy o 3, bo analizowane sa 3 sk³adowe w jednej iteracji 
-		dec rdx									; dekrementacja szerokoœci wiersza
+
+		add r15, 12								; przesuniêcie indeksu tablicy o 3, bo analizowane sa 3 sk³adowe w jednej iteracji 
+		sub rdx, 4								; dekrementacja szerokoœci wiersza
 		jnz widthLoop							; skok do pêtli wewnetrznej je¿eli szerokoœæ nie jest zerem
 	
 ;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
